a2dc5622c38abd11d6ce95508f3ac15935ddfc13
lakshmiramki2008-dotcom <lakshmiramki2008@gmail.com>
2026-01-18 15:27:42 +0530

    Update Storage classes.C

--- C-programs/Storage classes.C
+++ C-programs/Storage classes.C
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
-int latefee=15;
+int latefee=15;            //global variable
 void main()
 {
 auto int bkid=10;           //auto variable
 register int Studentid=20;  //register variable
 static int bkissued=9;      //static variable
-extern int latefee;         //extern variable
+extern int latefee;         //References the global variable
 clrscr();
 printf("Book Id: %d\n",bkid);
 printf("Student Id:%d\n",Studentid);
@@ -14,5 +14,6 @@ printf("Book Issued:%d\n",bkissued);
 printf("Late Fee:%d\n",latefee);
 getch();
 }
+
 
 
