dc0ad7612f9950505c55639ebaf5a0b64c2ba894
lakshmiramki2008-dotcom <lakshmiramki2008@gmail.com>
2026-01-18 15:10:35 +0530

    Update Strings.c
    concatenate the string

--- Strings.c
+++ Strings.c
@@ -1,11 +1,23 @@
+1,copy the contents in the string
 #include<stdio.h>
 #include<string.h>
 int main()
 {
-
     char str[100],dest[100];
     scanf("%s",&str);
     strcpy(dest,str);
     printf("Copied string: %s",dest);
+    return 0;
+}
+2,Concatenate the string
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[100],str2[100];
+    scanf("%s",&str1);
+    scanf("%s",&str2);
+    strcat(str1,str2);
+    printf("Concatenated string: %s",str1);
     return 0;
 }
